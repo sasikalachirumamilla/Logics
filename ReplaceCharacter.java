40eea8a193ac8baebd419029af491cbfd349c297
Sasikala Chirumamilla <sasichirumamilla22@gmail.com>
2024-07-23 12:09:58 +0530

    Basic Code

--- ReplaceCharacter.java
+++ ReplaceCharacter.java
@@ -11,6 +11,7 @@ void task()
 {
 System.out.println("Enter any String");
 str=sc.nextLine();
+
 System.out.println("enter index value and character you want to replace");
 int index1=sc.nextInt();
 index=sc.nextInt();
@@ -18,8 +19,9 @@ letter=sc.next();
 
 StringBuilder sb=new StringBuilder(str);
 sb.replace(index1, index, letter);
- //System.out.println(sb);
+System.out.println(sb);
 
+/*
 if (index >= 0 && index < sb.length()) 
 {
 sb.setCharAt(index1,index ,letter);
@@ -27,9 +29,9 @@ str = sb.toString();
 System.out.println("Updated String: " + str);
 } else {
 System.out.println("Index out of bounds");
-}
-}
+}*/
 
+}
 public static void main(String...args)
 {
 ReplaceCharacter rc=new ReplaceCharacter();
